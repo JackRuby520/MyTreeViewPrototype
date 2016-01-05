ffeec76d4d8848ffc24e6bf2bbb203bc4c541a39
JackRuby520 <820075@pchome.com.tw>
2016-01-05 15:37:02 +0800

    Update main.m

--- main.m
+++ main.m
@@ -13,5 +13,6 @@ int main(int argc, char *argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
+    System.out.println(pool);
     return retVal;
 }
